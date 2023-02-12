0ae20cabb72dd06f1456ab82a5604b985546ecf1
Ilya <ilyakudrov@yandex.ru>
2023-02-12 19:21:56 +0300

    added gpu test functions for plaket

--- lib/gpu/include/observables/plaket.h
+++ lib/gpu/include/observables/plaket.h
@@ -4,21 +4,41 @@
 #include "../general/matrix.h"
 #include "stdio.h"
 
+__constant__ unsigned int steps[4];
+__constant__ unsigned int arr_mu[6];
+__constant__ unsigned int arr_nu[6];
+
 namespace plaket_gpu {
-__global__ void plaket(matrix_gpu::su3 *conf, double *traces, int *indices);
-__device__ double plaket_site(matrix_gpu::su3 *conf, int *indices, int mu,
-                              int nu);
-__host__ void make_plaket_indices(int *indices, int lattice_size[4]);
+__global__ void plaket(matrix_gpu::su3 *conf, double *traces,
+                       unsigned int *indices);
+__device__ double plaket_site(matrix_gpu::su3 *conf, unsigned int *indices,
+                              unsigned short int mu, unsigned short int nu);
+__host__ void make_plaket_indices(unsigned int *indices, int lattice_size[4]);
 
 __global__ void __launch_bounds__(128, 4)
-    plaket_test1(matrix_gpu::su3 *conf, double *traces, int *indices);
+    plaket_test1(matrix_gpu::su3 *conf, double *traces, unsigned int *indices);
 
-__global__ void plaket(matrix_gpu::su3 *conf, double *traces, int *indices) {
+__device__ double plaket_site_test2(const matrix_gpu::su3 *conf,
+                                    const unsigned int *indices);
+
+__global__ void __launch_bounds__(128, 4)
+    plaket_test2(const matrix_gpu::su3 *conf, double *traces,
+                 const unsigned int *indices);
+
+__host__ void make_plaket_indices_test2(unsigned int *indices,
+                                        int lattice_size[4]);
+
+__global__ void __launch_bounds__(128, 4)
+    plaket_test3(const matrix_gpu::su3 *conf, double *traces);
+
+__global__ void __launch_bounds__(128, 6)
+    plaket_test4(matrix_gpu::su3 *conf, double *traces, unsigned int *indices);
+
+__global__ void plaket(matrix_gpu::su3 *conf, double *traces,
+                       unsigned int *indices) {
   int lat_index = blockIdx.x * blockDim.x + threadIdx.x;
-  int *ind_tmp = &indices[lat_index * 5];
+  unsigned int *ind_tmp = &indices[lat_index * 5];
   double trace = 0;
-
-  // __shared__ su3 A1 = conf[0];
 
   trace += plaket_site(conf, ind_tmp, 0, 1);
   trace += plaket_site(conf, ind_tmp, 0, 2);
@@ -29,14 +49,14 @@ __global__ void plaket(matrix_gpu::su3 *conf, double *traces, int *indices) {
   traces[lat_index] = trace;
 }
 
-__device__ double plaket_site(matrix_gpu::su3 *conf, int *indices, int mu,
-                              int nu) {
+__device__ double plaket_site(matrix_gpu::su3 *conf, unsigned int *indices,
+                              unsigned short int mu, unsigned short int nu) {
   matrix_gpu::su3 A = conf[indices[0] + mu] * conf[indices[mu + 1] + nu];
   A = A ^ conf[indices[nu + 1] + mu];
   return A.multiply_tr(conf[indices[0] + nu]);
 }
 
-__host__ void make_plaket_indices(int *indices, int lattice_size[4]) {
+__host__ void make_plaket_indices(unsigned int *indices, int lattice_size[4]) {
   link1 link(lattice_size[0], lattice_size[1], lattice_size[2],
              lattice_size[3]);
   int place = 0;
@@ -59,12 +79,12 @@ __host__ void make_plaket_indices(int *indices, int lattice_size[4]) {
   }
 }
 
-__global__ void plaket_test1(matrix_gpu::su3 *__restrict__ conf, double *traces,
-                             int *indices) {
-  int lat_index = blockIdx.x * blockDim.x + threadIdx.x;
-  int plaket_num = lat_index % 6;
-  int mu = 0;
-  int nu = 0;
+__global__ void plaket_test1(matrix_gpu::su3 *conf, double *traces,
+                             unsigned int *indices) {
+  unsigned int lat_index = blockIdx.x * blockDim.x + threadIdx.x;
+  unsigned int plaket_num = lat_index % 6;
+  unsigned short int mu = 0;
+  unsigned short int nu = 0;
   if (plaket_num == 0) {
     mu = 0;
     nu = 1;
@@ -86,6 +106,96 @@ __global__ void plaket_test1(matrix_gpu::su3 *__restrict__ conf, double *traces,
   }
 
   traces[lat_index] = plaket_site(conf, &indices[(lat_index / 6) * 5], mu, nu);
+}
+
+__device__ double plaket_site_test2(const matrix_gpu::su3 *conf,
+                                    const unsigned int *indices) {
+  matrix_gpu::su3 A = conf[indices[0]] * conf[indices[1]];
+  A = A ^ conf[indices[2]];
+  return A.multiply_tr(conf[indices[3]]);
+}
+
+__global__ void plaket_test2(const matrix_gpu::su3 *conf, double *traces,
+                             const unsigned int *indices) {
+  int lat_index = blockIdx.x * blockDim.x + threadIdx.x;
+
+  traces[lat_index] = plaket_site_test2(conf, &indices[lat_index * 4]);
+}
+
+__host__ void make_plaket_indices_test2(unsigned int *indices,
+                                        int lattice_size[4]) {
+  link1 link(lattice_size[0], lattice_size[1], lattice_size[2],
+             lattice_size[3]);
+  int place = 0;
+  for (int t = 0; t < lattice_size[0]; t++) {
+    for (int z = 0; z < lattice_size[0]; z++) {
+      for (int y = 0; y < lattice_size[0]; y++) {
+        for (int x = 0; x < lattice_size[0]; x++) {
+          link.go_update(x, y, z, t);
+          for (int mu = 0; mu < 3; mu++) {
+            for (int nu = mu + 1; nu < 4; nu++) {
+              indices[place] = link.place + mu;
+              place += 1;
+              link.move(mu, 1);
+              indices[place] = link.place + nu;
+              place += 1;
+              link.move(mu, -1);
+              link.move(nu, 1);
+              indices[place] = link.place + mu;
+              place += 1;
+              link.move(nu, -1);
+              indices[place] = link.place + nu;
+              place += 1;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+__global__ void plaket_test3(const matrix_gpu::su3 *conf, double *traces) {
+  int lat_index = blockIdx.x * blockDim.x + threadIdx.x;
+  int plaket_num = lat_index % 6;
+  int lat_site = (lat_index / 6) * 4;
+  unsigned int mu = arr_mu[plaket_num];
+  unsigned int nu = arr_nu[plaket_num];
+  unsigned int step_mu = steps[mu];
+  unsigned int step_nu = steps[nu];
+
+  matrix_gpu::su3 A = conf[lat_site + mu] * conf[lat_site + step_mu + nu];
+  A = A ^ conf[lat_site + step_nu + mu];
+  traces[lat_index] = A.multiply_tr(conf[lat_site + nu]);
+}
+
+__global__ void plaket_test4(matrix_gpu::su3 *conf, double *traces,
+                             unsigned int *indices) {
+  unsigned int lat_index =
+      blockIdx.x * blockDim.x * blockDim.y + blockDim.x * threadIdx.y;
+  unsigned short int mu = 0;
+  unsigned short int nu = 0;
+  if (threadIdx.x == 0) {
+    mu = 0;
+    nu = 1;
+  } else if (threadIdx.x == 1) {
+    mu = 0;
+    nu = 2;
+  } else if (threadIdx.x == 2) {
+    mu = 0;
+    nu = 3;
+  } else if (threadIdx.x == 3) {
+    mu = 1;
+    nu = 2;
+  } else if (threadIdx.x == 4) {
+    mu = 1;
+    nu = 3;
+  } else if (threadIdx.x == 5) {
+    mu = 2;
+    nu = 3;
+  }
+
+  traces[lat_index + threadIdx.x] =
+      plaket_site(conf, &indices[(lat_index / 6) * 5], mu, nu);
 }
 
 } // namespace plaket_gpu
--- tests/gpu/code/plaket.cu
+++ tests/gpu/code/plaket.cu
@@ -35,10 +35,10 @@ using namespace std;
 
 int main() {
 
-  x_size = 36;
-  y_size = 36;
-  z_size = 36;
-  t_size = 36;
+  x_size = NX;
+  y_size = NY;
+  z_size = NZ;
+  t_size = NT;
 
   int lattice_size = x_size * y_size * z_size * t_size;
 
@@ -60,14 +60,16 @@ int main() {
                        lattice_size * 4 * sizeof(MATRIX_TYPE_GPU),
                        cudaMemcpyHostToDevice));
 
-  int *lat_indices = (int *)malloc(lattice_size * 9 * sizeof(int));
+  unsigned int *lat_indices =
+      (unsigned int *)malloc(lattice_size * 9 * sizeof(unsigned int));
   int lattice_sizes[4] = {NX, NY, NZ, NT};
   plaket_gpu::make_plaket_indices(lat_indices, lattice_sizes);
 
-  int *dlat_indices;
-  gpuErrchk(cudaMalloc(&dlat_indices, lattice_size * 9 * sizeof(int)));
+  unsigned int *dlat_indices;
+  gpuErrchk(cudaMalloc(&dlat_indices, lattice_size * 9 * sizeof(unsigned int)));
   gpuErrchk(cudaMemcpy(dlat_indices, lat_indices,
-                       lattice_size * 9 * sizeof(int), cudaMemcpyHostToDevice));
+                       lattice_size * 9 * sizeof(unsigned int),
+                       cudaMemcpyHostToDevice));
 
   double *dtraces;
   gpuErrchk(cudaMalloc(&dtraces, lattice_size * sizeof(double)));
@@ -129,7 +131,7 @@ int main() {
   gpuErrchk(cudaPeekAtLastError());
   gpuErrchk(cudaDeviceSynchronize());
 
-  cout << "kernal plaket time " << milliseconds / 1000 << endl;
+  cout << "kernal plaket_test1 time " << milliseconds / 1000 << endl;
 
   traces = (double *)malloc(lattice_size * 6 * sizeof(double));
   gpuErrchk(cudaMemcpy(traces, dtraces, lattice_size * 6 * sizeof(double),
@@ -140,10 +142,139 @@ int main() {
     plaket += traces[i];
   }
 
-  cout << "plaket aver " << plaket / lattice_size / 6 << endl;
+  cout << "plaket_test1 aver " << plaket / lattice_size / 6 << endl;
 
   gpuErrchk(cudaFree(dtraces));
   free(traces);
+
+  // plaket_test4
+
+  gpuErrchk(cudaMalloc(&dtraces, lattice_size * 6 * sizeof(double)));
+  cudaEventRecord(start);
+
+  dim3 dimGrid(6, 16);
+  cout << "number of blocks " << (NX * NY * NZ * NT * 6 + 6 * 16 - 1) / (6 * 16)
+       << endl;
+
+  plaket_gpu::plaket_test4<<<(NX * NY * NZ * NT * 6 + 6 * 16 - 1) / (6 * 16),
+                             dimGrid>>>(dconf, dtraces, dlat_indices);
+
+  cudaEventRecord(stop);
+
+  gpuErrchk(cudaEventSynchronize(stop));
+  milliseconds = 0;
+  cudaEventElapsedTime(&milliseconds, start, stop);
+
+  gpuErrchk(cudaPeekAtLastError());
+  gpuErrchk(cudaDeviceSynchronize());
+
+  cout << "kernal plaket_test4 time " << milliseconds / 1000 << endl;
+
+  traces = (double *)malloc(lattice_size * 6 * sizeof(double));
+  gpuErrchk(cudaMemcpy(traces, dtraces, lattice_size * 6 * sizeof(double),
+                       cudaMemcpyDeviceToHost));
+
+  plaket = 0;
+  for (int i = 0; i < lattice_size * 6; i++) {
+    plaket += traces[i];
+  }
+
+  cout << "plaket_test4 aver " << plaket / lattice_size / 6 << endl;
+
+  gpuErrchk(cudaFree(dlat_indices));
+  free(lat_indices);
+
+  // plaket_test2
+
+  lat_indices =
+      (unsigned int *)malloc(lattice_size * 24 * sizeof(unsigned int));
+  plaket_gpu::make_plaket_indices_test2(lat_indices, lattice_sizes);
+
+  gpuErrchk(
+      cudaMalloc(&dlat_indices, lattice_size * 24 * sizeof(unsigned int)));
+  gpuErrchk(cudaMemcpy(dlat_indices, lat_indices,
+                       lattice_size * 24 * sizeof(unsigned int),
+                       cudaMemcpyHostToDevice));
+
+  cudaEventRecord(start);
+
+  plaket_gpu::plaket_test2<<<6 * BLOCK_NUMBER, THREADS_PER_BLOCK>>>(
+      dconf, dtraces, dlat_indices);
+
+  cudaEventRecord(stop);
+
+  gpuErrchk(cudaEventSynchronize(stop));
+  milliseconds = 0;
+  cudaEventElapsedTime(&milliseconds, start, stop);
+
+  gpuErrchk(cudaPeekAtLastError());
+  gpuErrchk(cudaDeviceSynchronize());
+
+  cout << "kernal plaket_test2 time " << milliseconds / 1000 << endl;
+
+  gpuErrchk(cudaMemcpy(traces, dtraces, lattice_size * 6 * sizeof(double),
+                       cudaMemcpyDeviceToHost));
+
+  plaket = 0;
+  for (int i = 0; i < lattice_size * 6; i++) {
+    plaket += traces[i];
+  }
+
+  cout << "plaket_test2 aver " << plaket / lattice_size / 6 << endl;
+
+  free(lat_indices);
+  gpuErrchk(cudaFree(dlat_indices));
+  gpuErrchk(cudaFree(dconf));
+
+  // plaket_test3
+
+  unsigned int hsteps[4] = {4, 4 * NX, 4 * NX * NY, 4 * NX * NY * NZ};
+  unsigned int harr_mu[6] = {0, 0, 0, 1, 1, 2};
+  unsigned int harr_nu[6] = {1, 2, 3, 2, 3, 3};
+
+  gpuErrchk(cudaMemcpyToSymbol(steps, hsteps, 4 * sizeof(unsigned int)));
+  gpuErrchk(cudaMemcpyToSymbol(arr_mu, harr_mu, 6 * sizeof(unsigned int)));
+  gpuErrchk(cudaMemcpyToSymbol(arr_nu, harr_nu, 6 * sizeof(unsigned int)));
+
+  gpuErrchk(cudaMalloc(&dconf, lattice_size * 4 * 2 * sizeof(MATRIX_TYPE_GPU)));
+  conf.array.reserve(lattice_size * 4 * 2);
+  for (int i = 0; i < lattice_size * 4; i++) {
+    conf.array.push_back(conf.array[i]);
+  }
+  gpuErrchk(cudaMemcpy(dconf, &conf.array[0],
+                       lattice_size * 4 * 2 * sizeof(MATRIX_TYPE_GPU),
+                       cudaMemcpyHostToDevice));
+
+  cudaEventRecord(start);
+
+  plaket_gpu::plaket_test3<<<6 * BLOCK_NUMBER, THREADS_PER_BLOCK>>>(dconf,
+                                                                    dtraces);
+
+  cudaEventRecord(stop);
+
+  gpuErrchk(cudaEventSynchronize(stop));
+  milliseconds = 0;
+  cudaEventElapsedTime(&milliseconds, start, stop);
+
+  gpuErrchk(cudaPeekAtLastError());
+  gpuErrchk(cudaDeviceSynchronize());
+
+  cout << "kernal plaket_test3 time " << milliseconds / 1000 << endl;
+
+  gpuErrchk(cudaMemcpy(traces, dtraces, lattice_size * 6 * sizeof(double),
+                       cudaMemcpyDeviceToHost));
+
+  plaket = 0;
+  for (int i = 0; i < lattice_size * 6; i++) {
+    plaket += traces[i];
+  }
+
+  cout << "plaket_test3 aver " << plaket / lattice_size / 6 << endl;
+
+  gpuErrchk(cudaFree(dtraces));
+  free(traces);
+
+  gpuErrchk(cudaFree(dconf));
 
   // double *d_odata;
   // gpuErrchk(cudaMalloc(&d_odata, BLOCK_NUMBER * sizeof(double)));
